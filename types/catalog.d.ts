fb36faf8f07e593c98115412c9f3805ea376453a
iamayatulai <ayatulaiolamide@gmail.com>
2025-06-26 13:03:48 +0100

    implement catalog and product feching with UI component for the homepage

---
+++ types/catalog.d.ts
@@ -0,0 +1,12 @@
+
+import { IProduct } from "./product";
+
+export interface ICatalog {
+  id: number;
+  localizeInfos?: {
+    title?: string;
+  };
+  catalogProducts: {
+    items: IProduct[];
+  };
+}
